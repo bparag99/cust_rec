c21c951dd827abc2a4db31b30b093f7c88a9018c
Parag <52174439+bparag99@users.noreply.github.com>
2023-10-20 13:26:33 +0530

    Update db_to_api.py

--- db_to_api.py
+++ db_to_api.py
@@ -20,6 +20,24 @@ def fetch_customers():
     print(json)
 
     return json
-
+@app.route('/chats', methods=['GET'])
+@cross_origin()
+def find_all_chats():  
+    cwd = os.getcwd()
+    print(cwd+r'\chat')
+    # for file in os.listdir():
+    os.chdir(path)
+    chat_list = os.listdir()
+    # print(chat_list)
+    json_list = []
+    for i in chat_list:
+        name_date = {}
+        name_date['name'] = i.split('.')[0].split('_')[0]
+        name_date['date'] = i.split('.')[0].split('_')[1]
+        json_list.append(name_date)
+        # print(name_date)
+    json_chat_list = json.dumps(json_list)
+    print(json_chat_list)
+    return json_chat_list
 if __name__ == '__main__':
     app.run(debug=True,port=8001)
