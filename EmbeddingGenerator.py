9b5f7566fca4b00c94b996b584b4899280a4ef7c
Parag <52174439+bparag99@users.noreply.github.com>
2023-10-20 17:50:44 +0530

    Update EmbeddingGenerator.py

--- EmbeddingGenerator.py
+++ EmbeddingGenerator.py
@@ -52,47 +52,6 @@ class EmbeddingGenerator:
         print(persona_df.head(3))
         return persona_df  
 
-    def get_embedded_dataframe(self,persona_df):
-        
-        embedded_dict = {}
-
-        if(enable_caching):
-            try:
-                path = r'C:\Users\839152\Downloads\Gen AI\RecommendationSystem\caching'
-                # Change the directory
-                os.chdir(path)
-                # read pickle file
-                with open('embeddings.pkl', 'rb') as fp:
-                    embedded_dict = pickle.load(fp)
-                    # print('length : ',len(embedded_dict))
-                    print('Embeddings cache available')
-            except:
-                print('Embeddings cache not available')
-
-        # Text Normalisation
-        persona_df['customer360']= persona_df["customer360"].apply(lambda text : self.normalize_text(text))
-
-        # Tokenization
-        tokenizer = tiktoken.get_encoding("cl100k_base")
-        persona_df['n_tokens'] = persona_df["customer360"].apply(lambda x: len(x))
-        persona_df = persona_df[persona_df.n_tokens<8192]
-
-        new_embeddings = {}
-        for i, (hashid, customer360) in enumerate(zip(persona_df["hash_id"], persona_df["customer360"])):
-            vec = get_embedding(customer360, engine = self.deployment_name_embedding)
-            new_embeddings[hashid] = vec
-            if hashid not in embedded_dict:
-                print(i)
-                embedded_dict[hashid] = vec
-
-
-        # Storing Embedding cache
-        # save dictionary to embeddings.pkl file
-        with open('embeddings.pkl', 'wb') as fp:
-            pickle.dump(embedded_dict, fp)
-            print('New Embedding cache file created')
-        
-        return [embedded_dict,new_embeddings]
 
 
 
